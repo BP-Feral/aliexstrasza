8bad58305a8d151ea3f27aa94898c8e4d2549838
pricob <mihai.pricob@yahoo.com>
2020-11-02 23:13:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+
+client.user.setActivity(`Lmao no strem`, {
+type: "STREAMING",
+url: "https://www.twitch.tv/blackpigeon12"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
+});
+
+client.login('MjY4NzQ2ODA2NTIxODg4NzY4.X6Byew.QLSvq8jcXNFK5dyJUNcy2kQoQgc');

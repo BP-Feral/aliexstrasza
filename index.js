664d1d88badf9223dc584871ad8f55eff48356b3
pricob <mihai.pricob@yahoo.com>
2020-11-03 13:05:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ client.on("message", async msg => {
   if (command === "help") {
   	const helpembed = new Discord.RichEmbed()
   	.setColor("#7289DA")
-  	//.setAuthor(client.user.tag, client.user.displayAvatarURL())
-      message.author.avatarURL({ dynamic:true })
+  	.setAuthor(client.tag)
   	.setDescription(
   	  `
   	 __**My List**__

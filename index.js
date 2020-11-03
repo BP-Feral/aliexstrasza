e29b7f545f2de0ca3859a1252064d59b592c990c
pricob <mihai.pricob@yahoo.com>
2020-11-04 00:00:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,53 +25,27 @@ client.on("message", async msg => {
   	.setColor("#7289DA")
   //.setAuthor(client.user.tag, client.user.displayAvatarURL())
   	.setAuthor(client.user.tag)
+    //==========================DESCRIPTION==============================
   	.setDescription(`
-  	 __**Testing App**__
-  	        \`^-^\`This section is not finished yet!\`^-^\`
-     >My current commands are \`aliexhelp\` and \`aliexsummoner\`
-     \`^-^\`Contact me on private to suggest new functions!\`^-^\`      `)
+  	 __**Help Center**__
+  	 \`^-^\`This section is not finished yet!\`^-^\`
+     My current commands are \`aliexhelp\` and \`aliexsum\`
+     Contact me on private to suggest new functions!
+     I can even add userid-exclusive commands ;}`)
   		.setFooter("©️ 2020 AliexStrasza Development");
   	  msg.channel.send(helpembed);
   }
-  
+  //========================EXCLUSIVE COMMAND============================
   if ( command === "sum" && msg.author.id === "268746806521888768") {
-    msg.channel.send("My summoner name is \`Øphaquiłle\`");
+    msg.channel.send("My EUNE summoner name is \`Øphaquiłle\`");
      console.log(`Summoner name requested by AliexStrasza! -got reply-`);
   } else if ( command == "sum" && msg.author.id != "268746806521888768" )
              {
               console.log(`Summoner name requested, but wasn't the owner! -noreply-`);
              return
-}
-  //=============================================
-  if ( command === "test" && msg.author.id === "430833156095213568") {
-    msg.channel.send("Congrats! You passed!");
-    console.log(`derpols wrote something! -got reply-`);
-  } else if ( command == "test" && msg.author.id != "430833156095213568" )
-  {
-    console.log(`message -test- not sent by derpols!`);
-    msg.channel.send("I'm sorry, but only derpols is allowed to type that!");
-    return
   }
-   if ( command === "ter" && msg.author.id === "369531260831268864") {
-    msg.channel.send("Congrats! You passed!");
-    console.log(`TERFOLE wrote something! -got reply-`);
-  } else if ( command == "ter" && msg.author.id != "369531260831268864" )
-  {
-    console.log(`message -ter- not sent by TERFOLE! -got reply-`);
-    msg.channel.send("I'm sorry, but only TERFOLE is allowed to type that! ");
-    return
-  }
+  //========================EXCLUSIVE COMMAND============================
   
-  
-  //if (msg.author.client) { 
-   // if (msg.author.id == "268746806521888768") {
-  //	  if (command ==="summoner") { 
-    //    msg.channel.send("My summoner name is \`Øphaquiłle\`");
-    //  } 
-  //  } else {
-    //    msg.channel.send("Sorry, but only AliexStrasza can reveal his summoner name")
-  //  }
-//  }
-
+  //=========================ONLY WRITE ABOVE============================
 });
 client.login("MjY4NzQ2ODA2NTIxODg4NzY4.X6Byew.QLSvq8jcXNFK5dyJUNcy2kQoQgc");

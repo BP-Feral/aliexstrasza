6b79d9ee8bae1a79c43a8b03b861f38866d225a9
pricob <mihai.pricob@yahoo.com>
2020-12-13 18:58:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ client.on("message", async msg => {
   }
    if ( command === "txt" && msg.author.id === "268746806521888768") {
      console.log(`txt requested`);
+     const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+channel.send(exampleEmbed);
              return
   }
     if ( command === "opgg" && msg.author.id === "268746806521888768") {

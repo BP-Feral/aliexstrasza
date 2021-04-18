8ae8343944d8cb9af2179bf64a623347a253002f
pricob <mihai.pricob@yahoo.com>
2021-04-18 10:53:11 +0300

    fixed prefix in the command line

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const preffix = "na "
+const preffix = "na"
 
 client.on('ready', () => 
   console.log(`Logged in as ${client.user.tag}!`));
